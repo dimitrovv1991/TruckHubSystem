e4a848e58ac64ee5a452fb8c016cac47a99b88be
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-10 16:26:58 +0300

    Truck year manufactured custom attribute created

---
+++ TruckHubSystem.Infrastructure/Attributes/YearRangeAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TruckHubSystem.Infrastructure.Constants.DataConstants;
+
+namespace TruckHubSystem.Infrastructure.Attributes
+{
+    public class YearRangeAttribute : RangeAttribute
+    {
+        public YearRangeAttribute(int YearManifacturedMin)
+            :base(YearManifacturedMin,DateTime.Now.Year)
+        {
+            
+        }
+    }
+}
