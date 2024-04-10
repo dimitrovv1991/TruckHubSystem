bbc48f6052f2a2d25fbcd2d637f3da950059a30d
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-10 16:27:11 +0300

    data constants updated

---
+++ TruckHubSystem.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckHubSystem.Infrastructure.Constants
+{
+    public class DataConstants
+    {
+        public const int TransmissionTypeNameMaxLength = 10;
+
+
+        public const int LoadCategoryNameMaxLength = 100;
+
+
+        public const int BookingStatusNameMaxLength = 20;
+
+
+        public const int TruckManufacturerNameMaxLength = 50;
+        public const int TruckModelNameMaxLength = 100;
+        public const int TruckLicensePlateMaxLength = 30;
+        public const int YearManifacturedMin = 1960;
+    }
+}
---
+++ TruckHubSystem.Infrastructure/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckHubSystem.Infrastructure.Constants
+{
+    public class Messages
+    {
+        public const string YearManufacturedErrorMessage = "The year must be between 1960 and the current year!";
+    }
+}
