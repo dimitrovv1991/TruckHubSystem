cd7bfaeb189dd8420784d028c7fd676483639abd
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-13 16:16:04 +0300

    UniqueConstraint for driver phone Added

---
+++ TruckHubSystem.Infrastructure/Migrations/20240413095726_UniqueConstraintForDriverPhoneNumber.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TruckHubSystem.Infrastructure.Migrations
+{
+    public partial class UniqueConstraintForDriverPhoneNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_PhoneNumber",
+                table: "Drivers",
+                column: "PhoneNumber",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Drivers_PhoneNumber",
+                table: "Drivers");
+        }
+    }
+}
