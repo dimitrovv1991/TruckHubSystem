03e483b14a699be73b6640630695cb0b0ac8721b
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-13 16:19:38 +0300

    New Booking created

--- TruckHubSystem/Controllers/BookingController.cs
+++ TruckHubSystem/Controllers/BookingController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TruckHubSystem.Core.Contracts.Driver;
+using TruckHubSystem.Core.Contracts.Load;
+using TruckHubSystem.Core.Contracts.Truck;
 using TruckHubSystem.Core.Models.Booking;
+using TruckHubSystem.Infrastructure.Data;
 
 namespace TruckHubSystem.Controllers
 {
     [Authorize]
     public class BookingController : Controller
     {
+        private readonly ILoadService loadService;
+        private readonly ITruckService truckService;
+        private readonly IDriverService driverService;
+        private readonly TruckHubDbContext data;
+
+
+        public BookingController(ILoadService _loadService,
+            ITruckService _truckService,
+            IDriverService _driverService,
+            TruckHubDbContext _data)
+        {
+            loadService = _loadService;
+            truckService = _truckService;
+            driverService = _driverService;
+            data = _data;
+        }
+
         [HttpGet]
         public async Task<IActionResult> All()
         {
@@ -16,11 +37,33 @@ namespace TruckHubSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Details (int id)
+        public async Task<IActionResult> BookingSummary(int id)
         {
-            var model = new BookingDetailsViewModel();
+            var selectedLoadId = (int)TempData["SelectedLoadId"];
+            var selectedTruckId = (int)TempData["SelectedTruckId"];
+            var selectedDriverId = (int)TempData["SelectedDriverId"];
 
-            return View(model);
+
+            var loadTruckDriver = new List<int> { selectedLoadId, selectedTruckId, selectedDriverId};
+
+
+            var selectedLoad = await loadService.SelectLoadById(selectedLoadId);
+            var selectedTruck = await truckService.SelectedTruckById(selectedTruckId);
+            var selectedDriver = await driverService.SelectedDriverById(selectedDriverId);
+
+            var bookingModel = new BookingDetailsFormModel
+            {
+                LoadName = selectedLoad.Name,
+                LoadingFactoryName = selectedLoad.FactoryName,
+                LoadWeigth = selectedLoad.Weigth,
+                TruckManufacturer = selectedTruck.Manufacturer,
+                TruckImage = selectedTruck.ImageUrl,
+                TruckPlateNumber = selectedTruck.LicensePlate,
+                DriverFirstName = selectedDriver.FirstName,
+                DriverLastName = selectedDriver.FamilyName
+            };
+
+            return View(bookingModel);
         }
 
         [HttpGet]
@@ -32,10 +75,58 @@ namespace TruckHubSystem.Controllers
         [HttpPost]
         public async Task<IActionResult> Add(BookingFormModel model)
         {
-            return RedirectToAction(nameof(Details), new { id = 1 });
+            return RedirectToAction(nameof(BookingSummary), new { id = 1 });
         }
 
 
+        [HttpGet]
+        public async Task<IActionResult> SelectLoad ()
+        {
+            var loads = await loadService.AllAvailableLoadsAsync();
 
+            return View(loads);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SelectLoad(int selectedLoadId)
+        {
+            TempData["SelectedLoadId"] = selectedLoadId;
+
+            return RedirectToAction("SelectTruck");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SelectTruck()
+        {
+            var trucks = await truckService.AllAvailableTrucksAsync();
+
+            return View(trucks);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SelectTruck(int selectedTruckId)
+        {
+            TempData["SelectedTruckId"] = selectedTruckId;
+            TempData.Keep("SelectedLoadId");
+            return RedirectToAction("SelectDriver");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SelectDriver()
+        {
+            var drivers = await driverService.AllAvailableDriversAsync();
+
+            return View(drivers);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SelectDriver(int selectedDriverId)
+        {
+            TempData["SelectedDriverId"] = selectedDriverId;
+            TempData.Keep("SelectedLoadId");
+            TempData.Keep("SelectedTruckId");
+
+            return RedirectToAction("BookingSummary");
+        }
     }
 }
