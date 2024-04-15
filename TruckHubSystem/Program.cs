a0b3db14f5586d5ed8779dcca7f30a93bba15c86
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-15 17:05:33 +0300

    BookingService added

--- TruckHubSystem/Program.cs
+++ TruckHubSystem/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using TruckHubSystem.Core.Contracts.Booking;
 using TruckHubSystem.Core.Contracts.Driver;
 using TruckHubSystem.Core.Contracts.Factory;
 using TruckHubSystem.Core.Contracts.Load;
 using TruckHubSystem.Core.Contracts.Truck;
+using TruckHubSystem.Core.Services.Booking;
 using TruckHubSystem.Core.Services.Driver;
 using TruckHubSystem.Core.Services.Factory;
 using TruckHubSystem.Core.Services.Load;
@@ -22,6 +24,7 @@ builder.Services.AddScoped<IFactoryService, FactoryService>();
 builder.Services.AddScoped<ILoadService, LoadService>();
 builder.Services.AddScoped<ITruckService, TruckService>();
 builder.Services.AddScoped<IDriverService, DriverService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
 
 builder.Services.AddScoped<IRepository, Repository>();
 
