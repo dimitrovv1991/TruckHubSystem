4f01d6e16d3bd7888232eb42e96f90138530b069
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-15 17:06:26 +0300

    AddBooking added and adjusted

--- TruckHubSystem/Controllers/BookingController.cs
+++ TruckHubSystem/Controllers/BookingController.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using TruckHubSystem.Core.Contracts.Booking;
 using TruckHubSystem.Core.Contracts.Driver;
 using TruckHubSystem.Core.Contracts.Load;
 using TruckHubSystem.Core.Contracts.Truck;
 using TruckHubSystem.Core.Models.Booking;
+using TruckHubSystem.Core.Models.Factory;
 using TruckHubSystem.Infrastructure.Data;
+using TruckHubSystem.Infrastructure.Data.Models;
 
 namespace TruckHubSystem.Controllers
 {
@@ -14,17 +19,20 @@ namespace TruckHubSystem.Controllers
         private readonly ILoadService loadService;
         private readonly ITruckService truckService;
         private readonly IDriverService driverService;
+        private readonly IBookingService bookingService;
         private readonly TruckHubDbContext data;
 
 
         public BookingController(ILoadService _loadService,
             ITruckService _truckService,
             IDriverService _driverService,
+            IBookingService _bookingService,
             TruckHubDbContext _data)
         {
             loadService = _loadService;
             truckService = _truckService;
             driverService = _driverService;
+            bookingService = _bookingService;
             data = _data;
         }
 
@@ -37,7 +45,7 @@ namespace TruckHubSystem.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> BookingSummary(int id)
+        public async Task<IActionResult> BookingSummary()
         {
             var selectedLoadId = (int)TempData["SelectedLoadId"];
             var selectedTruckId = (int)TempData["SelectedTruckId"];
@@ -51,7 +59,7 @@ namespace TruckHubSystem.Controllers
             var selectedTruck = await truckService.SelectedTruckById(selectedTruckId);
             var selectedDriver = await driverService.SelectedDriverById(selectedDriverId);
 
-            var bookingModel = new BookingDetailsFormModel
+            var bookingModel = new BookingFormModel
             {
                 LoadName = selectedLoad.Name,
                 LoadingFactoryName = selectedLoad.FactoryName,
@@ -60,11 +68,33 @@ namespace TruckHubSystem.Controllers
                 TruckImage = selectedTruck.ImageUrl,
                 TruckPlateNumber = selectedTruck.LicensePlate,
                 DriverFirstName = selectedDriver.FirstName,
-                DriverLastName = selectedDriver.FamilyName
+                DriverLastName = selectedDriver.FamilyName,
+                SelectedDriverId = selectedDriverId,
+                SelectedLoadId = selectedLoadId,
+                SelectedTruckId = selectedTruckId
             };
 
             return View(bookingModel);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> BookingSummary(BookingFormModel bookingModel)
+        {
+            int selectedLoadId = bookingModel.SelectedLoadId;
+            int selectedTruckId = bookingModel.SelectedTruckId;
+            int selectedDriverId = bookingModel.SelectedDriverId;
+
+            var selectedLoad = await loadService.SelectLoadById(selectedLoadId);
+            var selectedTruck = await truckService.SelectedTruckById(selectedTruckId);
+            var selectedDriver = await driverService.SelectedDriverById(selectedDriverId);
+            
+            await bookingService.CreateBookingAsync(selectedLoad, selectedTruck, selectedDriver);
+
+            
+            return RedirectToAction("Index", "Home");
+        }
+
 
         [HttpGet]
         public IActionResult Add()
@@ -82,7 +112,9 @@ namespace TruckHubSystem.Controllers
         [HttpGet]
         public async Task<IActionResult> SelectLoad ()
         {
-            var loads = await loadService.AllAvailableLoadsAsync();
+            var userId = GetUserId();
+
+            var loads = await loadService.AllAvailableLoadsFromOtherUsersFactories(userId);
 
             return View(loads);
         }
@@ -128,5 +160,28 @@ namespace TruckHubSystem.Controllers
 
             return RedirectToAction("BookingSummary");
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Mine()
+        {
+            var currentUserId = GetUserId();
+
+
+            var bookings = await data.Bookings
+               .Select(b => new BookingDetailsFormModel()
+               {
+                   DriverFirstName = b.Driver.FirstName,
+                   DriverLastName = b.Driver.FamilyName,
+                   LoadName = b.Load.Name,
+
+               })
+               .Where(b => b.CreatorId == currentUserId)
+               .ToListAsync();
+
+            return View(factories);
+        }
+
+        private string GetUserId()
+            => User.FindFirstValue(ClaimTypes.NameIdentifier);
     }
 }
