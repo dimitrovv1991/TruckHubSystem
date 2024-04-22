fd15c33293d82907789c40c38280e193bc632d73
dimitrovv1991 <dimitrovv1991regs@gmail.com>
2024-04-22 17:01:04 +0300

    enums for booking sorting added

---
+++ TruckHubSystem.Core/Enums/BookingSorting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckHubSystem.Core.Enums
+{
+    public enum BookingSorting
+    {
+        Newest = 0,
+        Oldest = 1
+    }
+}
---
+++ TruckHubSystem.Core/Enums/BookingStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TruckHubSystem.Core.Enums
+{
+    public enum BookingStatus
+    {
+        All = 0,
+        InProgress = 1,
+        Finished = 2
+    }
+}
